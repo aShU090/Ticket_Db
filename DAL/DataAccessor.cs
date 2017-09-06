bda4f6e5462f61eb631384ae1013ed287e3d4850
APTTUS\ssingh <ssingh@apttus.com>
2017-09-06 09:15:10 +0530

    changed dataaccessor file

--- DAL/DataAccessor.cs
+++ DAL/DataAccessor.cs
@@ -1,34 +1,23 @@
 ﻿using Apttus.Assignment.MovieTicket;
-using Apttus.Assignment.MovieTicket.Enum;
+using Dapper;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Apttus.MovieTicket.DAL
 {
     public class DataAccessor : IDataAccessor
     {
-        public List<Persons> GetMembersDetails()
+        public List<Person> GetMembersDetails()
         {
-            List<Persons> personList = new List<Persons>();
-            string p_name = "";
-            int p_age = 0;
-            Gender p_gender;
-            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBPerson.mdf;Integrated Security=True";
-            //string cs =
-            //    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Data\BookTicket4\BookTicket\DAL\DBPerson.mdf;Integrated Security=True";
-            SqlConnection con = new SqlConnection(cs);
-            con.Open();
-            string query = "select * from Persons";
-            SqlCommand cmd1 = new SqlCommand(query, con);
-            SqlDataReader r = cmd1.ExecuteReader();
-            while (r.Read())
+            List<Person> personList = new List<Person>();
+
+            string sql = "SELECT * FROM Persons";
+            using (IDbConnection connection = new SqlConnection("Server=L005485;Initial Catalog=DbPerson;Integrated Security=true;"))
             {
-                p_name = r.GetString(0);
-                p_age = r.GetInt32(1);
-                p_gender = (Gender)r.GetInt32(2);
-                personList.Add(new Persons(p_name, p_age, p_gender));
+                var result = connection.Query<Person>(sql).AsList();
+                return result;
             }
-            return personList;
         }
     }
 }
